3a1fc4b600292f8a918eda699584f521ba5ce445
RushenA <rushena@MacBook-Air-RushenA.local>
2021-03-23 23:54:20 +0500

    Write function Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-    const bb = members.filter(i => typeof i !== 'string').map(i => i[0].toUpperCase()).sort((a, b) => a > b).join('');
-    console.log(bb);
-    return bb;
+    if (!(members instanceof Array)) return false;
+    return members.filter(i => typeof i == 'string').map(i => i.trim()[0].toUpperCase()).sort().join('');
 };
