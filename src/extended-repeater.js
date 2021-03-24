999557e5361f37af4fb2e5712ee018cda86a9416
RushenA <rushena@Air-RushenA.dyn.frg.m>
2021-03-24 20:42:20 +0500

    Write functions Extended Repeater and Hanoi Tower

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options;
+  let resultAddition = [];
+  let result = [];
+
+  repeatTimes = repeatTimes || 1;
+  separator = separator || '+';
+  addition = addition === undefined ? '' : addition;
+  additionRepeatTimes = additionRepeatTimes || 1;
+  additionSeparator = additionSeparator || '|';
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    resultAddition.push('' + addition);
+  }
+
+  resultAddition = resultAddition.join(additionSeparator);
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result.push('' + str + resultAddition);
+  }
+
+  return result.join(separator);
 };
   
