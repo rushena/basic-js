49de9324ce8be6603ae5d640a3d0ec09c3fb0f99
RushenA <rushena@MacBook-Air-RushenA.local>
2021-03-28 23:31:14 +0500

    Write function Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  const keys = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+  return arr.reduce((acc, i, k) => {
+    if (keys.includes(i)) {
+      return acc;
+    }
+
+    if (arr[k - 1] !== undefined && arr[k - 1] === '--discard-next') {
+      return acc;
+    }
+
+    if (arr[k + 1] !== undefined && arr[k + 1] === '--double-prev') {
+      acc.push(i)
+    }
+
+    if (arr[k - 1] !== undefined && arr[k - 1] === '--double-next') {
+      acc.push(i)
+    }
+
+    if (arr[k + 1] !== undefined && arr[k + 1] === '--discard-prev') {
+      return acc;
+    }
+
+    acc.push(i);
+    return acc;
+  }, []);
+}
